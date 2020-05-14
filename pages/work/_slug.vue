11b937697fbb876ebaac44c5a55af66b5eb00581
snsn1024 <asqqqqqqqqqq@gmail.com>
2020-05-15 02:29:17 +0900

    add slug-page

---
+++ pages/work/_slug.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <div 
+            class="w-full h-64 my-6 bg-cover bg-center shadow-lg"
+            :style=" 'background-image: url(' + work.fields.image.fields.file.url + ')' "
+        ></div>
+        <h1 class="text-center text-4xl">{{ work.fields.title }}</h1>
+        <p class="text-center text-sm">{{ work.fields.subtitle }}</p>
+        <div class="flex justify-center mb-5">
+            <li 
+                v-for="tag in work.fields.tag"
+                :key="tag.sys.id"
+                class="list-none text-xs m-1 bg-gray-200 p-1 rounded"
+            >
+                {{ tag.fields.name }}
+            </li>
+        </div>
+        <div class="my-10">
+            <p v-if="work.fields.url" class="text-xs">
+                <fa-layers full-width class="mr-1">
+                    <fa :icon="faLink" />
+                </fa-layers>
+                {{ work.fields.url }}
+            </p>
+            <p v-if="work.fields.gitHub" class="text-xs">
+                <fa-layers full-width class="mr-1">
+                    <fa :icon="faGithub" />
+                </fa-layers>
+                {{ work.fields.gitHub }}
+            </p>
+        </div>
+        <div
+            v-html="work.fields.content"
+        ></div>
+    </div>
+</template>
+
+<script>
+import { faLink } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+    import { createClient } from '~/plugins/contentful.js'
+    const client = createClient()
+    export default {
+        asyncData({params}) {
+            return Promise.all([
+                client.getEntries({
+                    'content.type': 'work',
+                    'fields.slug': params.slug
+                })
+            ]).then(([work]) => {
+                return {
+                    work: works.items[0]
+                }
+            }).catch(console.error)
+        },
+        computed: {
+            faLink() {
+                return faLink
+            },
+            faGithub() {
+                return faGithub
+            }
+        }
+    }
+</script>
